0c97734542b0a24edd7ae2ddf2670410ad48b820
ralph-p <mr.pereira91@gmail.com>
2023-06-12 11:03:04 -0400

    showing the equations

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # finite-automata
-A Finite Automata to solve mod 3
-## Getting Started
+Finite Automata(FA) is the simplest machine to recognize patterns. The finite automata or finite state machine is an abstract machine that has five elements or tuples. It has a set of states and rules for moving from one state to another but it depends upon the applied input symbol. Basically, it is an abstract model of a digital computer.
+Formal specification of machine is (Q, Σ, q, F, δ) 
+# Getting Started
 
 First, run the development server:
 
@@ -12,11 +13,33 @@ yarn dev
 ```
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
+# Features
+## Front End
+- Home page that breaks down what a FSM is
+- Mod 3 Example page
+    - An input where you can enter a binary string
+    - A visualizer to show the states what state you are currently on
+    - Table to show the transitions
+- Builder (under construction)
+    - Create all (Q, Σ, q, F, δ)  elements and run the FSM
+    - A visualizer to show the states what state you are currently on 
 ## API
+NextJS provides serverless API routes. 
 Once running you can use `GET http://localhost:3000/api/finite-automaton?states=S0,S1,S2&inputSymbols=0,1&initialState=S0&finalStates=S0,S1,S2&equations=S0,0,S0,1,S1,0,S1,1,S2,0,S2,1&matchingValues=S0,S1,S2,S0,S1,S2&inputString=100` to use the api to get the `state` and `value` for the `FSM`. 
 
-## Findings:
+You can also use `GET http://localhost:3000/api/mod-three?input=1011100` to run the `mod-three` example
+## Tests
+- Written tests for the FSM function, and coverage on all the pages 
+- Main components in Mod 3 are covered in tests
+# What I would add (2.0)
+- Fix styling 
+    - More Mobile Friendly (esp. svg rendering of states)
+    - Better theme   
+- Better testing
+- Clean up ID's and components
+- Seperate out the logic a bit more for creating the FSM
+- Draw lines between the states on the visualizer
+# Findings:
 
 ### 110111111101111011111111111111111111111101010101110000000000000000001 returning the incorrect response
 
--- client/components/FMEquation.tsx
+++ client/components/FMEquation.tsx
@@ -6,52 +6,66 @@ type EquationProps = {
   input: string;
   endingState: string;
 }
-const newEquation: EquationProps = {startingState: '', input: '', endingState: ''}
+const newEquation: EquationProps = { startingState: '', input: '', endingState: '' }
 export const FMEquation = () => {
-    const { contextFSM, updateContextFSM } = useFAContext()
-    const [equation, setEquation] = useState<EquationProps>(newEquation)
-    if(!contextFSM?.allStates) return <div></div>
-    const createEquation = () => {
-      const eq = [equation.startingState, equation.input, equation.endingState]
-      updateContextFSM('equation', eq)
-    }
-    return (
+  const { contextFSM, updateContextFSM } = useFAContext()
+  const [equation, setEquation] = useState<EquationProps>(newEquation)
+  if (!contextFSM?.allStates) return <div></div>
+  const createEquation = () => {
+    updateContextFSM('equation', [equation.startingState, equation.input, equation.endingState])
+  }
+  return (
+    <div className='flex flex-col'>
       <div className="grid grid-cols-4 gap-4">
-      <div>Starting State</div>
-      <div>Input</div>
-      <div>Final State State</div>
-      <div className=""></div>
-      <div>
-        {
-          Array.from(contextFSM.allStates).map((s) => (
-            <div key={`starting-state-rario-${s}`} className="flex items-center" >
-              <input id="interest1" type="radio"  name="equationStartingState" className="mr-2" checked={s === equation.startingState} onChange={() => setEquation({...equation, startingState: s})}/>
-              <label htmlFor="interest1" className="text-gray-700">{s}</label>
-            </div>
-          ))
-        }
+        <div>Starting State</div>
+        <div>Input</div>
+        <div>Final State State</div>
+        <div className=""></div>
+        <div>
+          {
+            Array.from(contextFSM.allStates).map((s) => (
+              <div key={`starting-state-rario-${s}`} className="flex items-center" >
+                <input id="interest1" type="radio" name="equationStartingState" className="mr-2" checked={s === equation.startingState} onChange={() => setEquation({ ...equation, startingState: s })} />
+                <label htmlFor="interest1" className="text-gray-700">{s}</label>
+              </div>
+            ))
+          }
+        </div>
+        <div>
+          {
+            Array.from(contextFSM.inputSymbols).map((i) => (
+              <div key={`input-rario-${i}`} className="flex items-center" >
+                <input id="interest1" type="radio" name="equationInputSymbol" className="mr-2" checked={i === equation.input} onClick={() => setEquation({ ...equation, input: i })} />
+                <label htmlFor="interest1" className="text-gray-700">{i}</label>
+              </div>
+            ))
+          }
+        </div>
+        <div>
+          {
+            Array.from(contextFSM.allStates).map((s) => (
+              <div key={`ending-state-rario-${s}`} className="flex items-center" >
+                <input id="interest1" type="radio" name="equationEndingState" className="mr-2" checked={s === equation.endingState} onClick={() => setEquation({ ...equation, endingState: s })} />
+                <label htmlFor="interest1" className="text-gray-700">{s}</label>
+              </div>
+            ))
+          }
+        </div>
+        <button className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800' onClick={createEquation}>Create Rule</button>
       </div>
-      <div>
-        {
-          Array.from(contextFSM.inputSymbols).map((i) => (
-            <div key={`input-rario-${i}`} className="flex items-center" >
-              <input id="interest1" type="radio" name="equationInputSymbol" className="mr-2" checked={i === equation.input} onClick={() => setEquation({...equation, input: i})} />
-              <label htmlFor="interest1" className="text-gray-700">{i}</label>
-            </div>
-          ))
-        }
-      </div>
-      <div>
-        {
-          Array.from(contextFSM.allStates).map((s) => (
-            <div key={`ending-state-rario-${s}`} className="flex items-center" >
-              <input id="interest1" type="radio" name="equationEndingState" className="mr-2" checked={s === equation.endingState} onClick={() => setEquation({...equation, endingState: s})} />
-              <label htmlFor="interest1" className="text-gray-700">{s}</label>
-            </div>
-          ))
-        }
-      </div>
-      <button onClick={createEquation}>Create Rule</button>
+      {
+        contextFSM?.equations.length && contextFSM?.matchingValues.length ? (
+          <div className='flex flex-row space-x-4 flex-wrap max-w-lg'>
+            {
+              contextFSM?.equations.map((equation, index) => {
+                return (
+                  <code>{`δ(${equation[0]}, ${equation[1]}) = ${contextFSM.matchingValues[index]};`}</code>
+                )
+              })
+            }
+          </div>
+        ) : null
+      }
     </div>
-    )
+  )
 }
