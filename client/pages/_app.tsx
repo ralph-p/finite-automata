8b49cae4460d6cd4d203154fd6207bc760b5a86a
ralph-p <mr.pereira91@gmail.com>
2023-06-13 13:25:30 -0400

    added analytics

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -3,6 +3,7 @@ import { Context } from '@/context/context'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -13,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
         </Head>
         <NavBar />
         <Component {...pageProps} />
+        <Analytics />
       </Context>
     </>
   )
